ca9375c1686b3c63e2b5a35bfbe76c60f4e80cf5
ansasa2122daw2 <ansasa2122daw2@protonmail.com>
2022-04-28 17:44:13 +0200

    Commit implementacion tipografia

--- script.js
+++ script.js
@@ -11,30 +11,30 @@ window.addEventListener("load", init);
 
 //tengo los parafos hardcoded
 const parafos = [
-	"¡Ah! ¡Qué terrible es el sino de los pobres locos, esos 'renglones torcidos', esos yerros, esas faltas de ortografía del Creador, como los llamaba 'el Autor de la Teoría de los Nueve Mundos', ignorante de que él era uno de los más torcidos de todos los renglones de la caligrafía divina!",
 	"Vida antes que muerte. ¿Qué significaba el dicho? ¿Que los hombres deberían buscar la vida antes que buscar la muerte? Eso era obvio. ¿O significaba otra cosa? ¿Que la vida venía antes que la muerte? Una vez más, obvio. Y sin embargo las palabras sencillas le hablaban. La muerte viene, susurraban. La muerte les viene a todos. Pero la vida viene primero. Saboréala. La muerte es el destino. Pero el viaje, eso es la vida. Eso es lo que importa.",
 	"Hay dos tipos de personas en el mundo, hijo -dijo su padre severamente-. Los que salvan vidas. Y los que las quitan. - ¿Y los que protegen y defienden? ¿Los que salvan vidas quitando vidas? Su padre bufó. -Eso es como intentar detener una tormenta soplando más fuerte. Ridículo. No se puede proteger matando.",
 	"Me quedé dormido pensando en que Amelia Garayoa, aquella misteriosa antepasada mía, había sido una romántica temperamental, una mujer ansiosa de experiencias, constreñida por las imposiciones sociales de su época; un tanto incauta y desde luego con una clara tendencia a la fascinación por el abismo.",
 	"Si el líder dice de tal evento esto no ocurrió, pues no ocurrió. Si dice que dos y dos son cinco, pues dos y dos son cinco. Esta perspectiva me preocupa mucho más que las bombas.",
-	" Cada libro, cada volumen que ves aquí, tiene un alma. El alma de la persona que lo escribió y de aquellos que lo leyeron, vivieron y soñaron con él. Cada vez que un libro cambia de manos, cada vez que alguien baja sus ojos a las páginas, su espíritu crece y se fortalece",
-	"Desde que nacimos... Todos nosotros... ¡Somos libres! No importa que tan fuertes sean quienes nos lo impidan...¡Lucha! ¡Si es por eso, no me importa morir! ¡No importa lo terrible que sea este mundo!... ¡No importa que tan cruel sea! ¡¡Lucha!! ",
+	"Cada libro, cada volumen que ves aquí, tiene un alma. El alma de la persona que lo escribió y de aquellos que lo leyeron, vivieron y soñaron con él. Cada vez que un libro cambia de manos, cada vez que alguien baja sus ojos a las páginas, su espíritu crece y se fortalece",
 	"Rendirse es lo que destruye a la gente, cuando te niegas con todo tu corazón a rendirte entonces trasciendes tu humanidad, incluso ante la muerte nunca te rindas.",
 ];
 
 //DOM
-
 let frases = document.getElementById("randomize");
 let input = document.getElementById("input");
 let timer = document.getElementById("time");
+let solucionDIV = document.getElementById("body");
+let texto = document.getElementById("typing-texto");
 
 //eventos
 input.addEventListener("input", () => {
 	console.log("cambia?");
+
 	let frasesSpan = frases.querySelectorAll("span");
 	let arrayValue = input.value.split("");
 	frasesSpan.forEach((characterS, index) => {
 		let character = arrayValue[index];
-		if (character === null || undefined) {
+		if (character === null) {
 			characterS.classList.remove("incorrect");
 			characterS.classList.remove("correct");
 		}
@@ -48,19 +48,23 @@ input.addEventListener("input", () => {
 	});
 });
 
-input.addEventListener("mouseover", () => {
-	//timer
-	setInterval(countdown, 1000);
-});
+//Cuando clickas empieza el contador
+input.addEventListener(
+	"input",
+	() => {
+		//timer
+		setInterval(countdown, 1000);
+	},
+	{ once: true }
+);
 
 //declaro
-let time = 10;
+let time = 30;
 let jugador = true;
 
 function init() {
 	muestra(parafos);
 	console.log("funciona?");
-
 	//si timer 0 entonces:
 	setInterval(check, 50);
 }
@@ -74,6 +78,7 @@ function muestra(parafos) {
 		const characterS = document.createElement("span");
 		characterS.innerText = character;
 		frases.appendChild(characterS);
+		characterS.classList.add("fuenteparrafo");
 	});
 }
 
@@ -83,7 +88,6 @@ function countdown() {
 	}
 	if (time === 0) {
 		jugador = false;
-		console.log("jeje");
 	}
 	timer.innerHTML = time;
 }
@@ -91,5 +95,12 @@ function countdown() {
 function check() {
 	if (!jugador && time === 0) {
 		input.disabled = "true";
+
+		//acabar y enseñar stats
+		let name = "<div>Resultados: </div>";
+		let estilosS = (solucionDIV.innerHTML = name);
+		estilosS.style.width = "20%";
+		estilosS.style.height = "20%";
+		estilosS.style.backgroundColor = "grey";
 	}
 }
